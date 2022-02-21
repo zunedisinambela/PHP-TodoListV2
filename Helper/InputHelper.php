8dca844d964003bff5af5f6a4f4b35d8f587fcc8
zunedisinambela <zunedisinambela@gmail.com>
2022-02-21 19:11:01 +0700

    View: Menampilkan Todolist

---
+++ Helper/InputHelper.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Helper {
+
+    class InputHelper {
+
+        static function input(string $info): string
+        {
+            echo "$info : ";
+            $result = fgets(STDIN);
+            return trim($result);
+        }
+    }
+}
--- View/TodolistView.php
+++ View/TodolistView.php
@@ -2,11 +2,42 @@
 
 namespace View {
 
-    class TodolistView
+    use Service\TodolistService;
+    use Helper\InputHelper;
+
+class TodolistView
     {
+        private TodolistService $todolistService;
+
+        public function __construct(TodolistService $todolistService)
+        {
+            $this->todolistService = $todolistService;
+        }
+
         function showTodolist(): void
         {
-            //
+            while (true) {
+                $this->todolistService->showTodolist();
+
+                echo "MENU" . PHP_EOL;
+                echo "1. Tambah Todo" . PHP_EOL;
+                echo "2. Hapus Todo" . PHP_EOL;
+                echo "x. Keluar" . PHP_EOL;
+
+                $pilihan = InputHelper::input("Pilih");
+
+                if ($pilihan == "1") {
+                    $this->addTodolist();
+                } else if($pilihan == "2") {
+                    $this->removeTodolist();
+                } else if($pilihan == "x") {
+                    break;
+                } else {
+                    echo "Pilihan tidak dimengerti" . PHP_EOL;
+                }
+            }
+
+            echo "Sampai Jumpa Lagi" . PHP_EOL;
         }
 
         function addTodolist(): void
