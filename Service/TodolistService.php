07d862a5e464817af76723109d5f4f8c04c722e0
zunedisinambela <zunedisinambela@gmail.com>
2022-02-21 08:33:49 +0700

    Membuat Service

---
+++ Service/TodolistService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Service {
+
+    interface TodolistService
+    {
+        function showTodolist(): void;
+
+        function addTodolist(string $todo): void;
+
+        function removeTodolist(int $number): void;
+    }
+}
